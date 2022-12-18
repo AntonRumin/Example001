897e542b1808744346af7766225f05a3da4269cb
Anton_R <anton.spb.ru@gmail.com>
2022-12-19 01:29:17 +0300

    Второй пример лекции. Приветвие пользователю

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿// Второй пример из лекции 1.Приветствие пользователю
+
+Console.Write ("Введите ваше имя");
+
+string username = Console.ReadLine();
+
+Console.Write ("Привет,");
+
+Console.WriteLine (username);
