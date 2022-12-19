9047dfc5c8fbf90293f6bf03015799afd886fbb3
Anton_R <anton.spb.ru@gmail.com>
2022-12-20 00:25:40 +0300

    Add If-Else Algoritm

---
+++ Example006_BaseWeight/Program.cs
@@ -0,0 +1,12 @@
+﻿// пример 6 Лекция 3_1. Выбор максимального веса
+int a = 2;
+int b = 5;
+int c = 9;
+int d = 3;
+int max = 0;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+Console.Write("максимальное значение (кг):");
+Console.Write(max);
