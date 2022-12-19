07d81f3a4d1c64c42a0496f54ca64590cef2f06a
Anton_R <anton.spb.ru@gmail.com>
2022-12-19 23:33:45 +0300

    Add Random number to Exmple 003

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,8 @@
 ﻿// пример 3 лекции 3_1. Сложение двух чисел
-int numberOne = 81;
+int numberOne = 86;
 int numberTwo = 96;
 int result = numberOne + numberTwo;
-
+int randomResult = new Random().Next (1, numberOne) + new Random().Next (1,numberTwo);
 
 Console.WriteLine(result);
+Console.WriteLine(randomResult);
