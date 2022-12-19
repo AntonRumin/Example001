73a433778cfedd10637bd17ae81ef59435b69fb6
Anton_R <anton.spb.ru@gmail.com>
2022-12-20 00:07:10 +0300

    Add specil Hello for one user

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "антон")
+{
+    Console.WriteLine ("Привет, Брат!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine(userName);
+}
