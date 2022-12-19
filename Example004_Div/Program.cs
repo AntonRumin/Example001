47ea0cfc3b13ae55e795a02104e82a1f53c2d9c4
Anton_R <anton.spb.ru@gmail.com>
2022-12-19 12:53:28 +0300

    Add Example 004 Division

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,12 @@
+﻿// пример 4 лекции 3_1.Деление двух чисел
+int numberOne = 81;
+int numberTwo = 96;
+int result1 = numberOne/numberTwo;
+int result2 = numberOne%numberTwo;
+
+Console.Write("Деление, целая часть ");
+
+Console.WriteLine(result1);
+
+Console.Write("Деление, остаток ");
+Console.WriteLine(result2);
