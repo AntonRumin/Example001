2a25953fe02d5abf1e6e743f590658c9422408ec
Anton_R <anton.spb.ru@gmail.com>
2022-12-21 23:48:43 +0300

    Add Seminar's code Square

---
+++ Seminar001_Square/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа для определения квадрата числа: получает число и возвращает квадрат этого числа
+/* Объявить переменные
+Получить число с консоли
+Преобразовать число
+Возвести в квадрат
+вернуть результат
+*/
+//Console.WriteLine("Hello, World!");
+
+Console.Write("Введите целое число ");
+string numberStr = Console.ReadLine() ?? "";
+int number = int.Parse(numberStr);
+int result = number*number;
+Console.WriteLine($"Квадрат числа {number} равен {result}");
