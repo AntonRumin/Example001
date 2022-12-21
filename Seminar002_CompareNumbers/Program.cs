c8f4436deec730744a13196c5f4640e66833dbf7
Anton_R <anton.spb.ru@gmail.com>
2022-12-22 00:27:24 +0300

    Add code Compare Numbers

---
+++ Seminar002_CompareNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿// Gрограмма: принимает два числа и определяет не явлляется ли одно из них квдратом второго.
+// Объявить переменные, ввести числа, сравнить числа, вывести результат
+
+Console.Clear();
+Console.Write ("Введите первое число  ");
+string stringOne = Console.ReadLine() ?? "";
+
+Console.Write ("Введите второе число  ");
+string stringTwo = Console.ReadLine() ?? "";
+
+double numberOne = double.Parse(stringOne), 
+       numberTwo = double.Parse(stringTwo);
+
+if (numberOne == numberTwo*numberTwo)
+{
+    Console.WriteLine($"Число {numberOne} является квадратом числа {numberTwo}");
+}
+if (numberTwo == numberOne*numberOne)
+{
+    Console.WriteLine($"Число {numberTwo} является квадратом числа {numberOne}");
+}
+if (numberOne != numberTwo*numberTwo && numberTwo != numberOne*numberOne)
+{
+    Console.WriteLine("Числа не являются квадратами друг друга");
+}
