f29356682010516500ce149af4dea5c407b1ee38
Anton_R <anton.spb.ru@gmail.com>
2022-12-22 10:40:01 +0300

    Add code Numbers from 1 to N

---
+++ Seminar004_NumbeRange/Program.cs
@@ -0,0 +1,17 @@
+﻿//Программа для определения всех чисел от 1 до ввведенного числа"
+Console.Clear();
+Console.WriteLine("Вывод чисел от 1 до N");
+
+Console.Write ("Введите любое целое число, N = ");
+
+int Num = int.Parse(Console.ReadLine() ?? "0");
+
+int count = 0;
+
+while (count < Num)
+{
+count = count + 1;
+
+Console.Write($"{count}, ");
+}
+
