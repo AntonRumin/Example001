3da457ea5497042e1ee10c369e1f7f3b983bc366
Anton_R <anton.spb.ru@gmail.com>
2022-12-22 09:12:22 +0300

    Add code Name of Day according to Number

---
+++ Seminar003_NameDay/Program.cs
@@ -0,0 +1,46 @@
+﻿//Программа для определения названия дня недели по номеру от 1 до 7"
+Console.Clear();
+Console.WriteLine("Определение названия дня недели");
+
+Console.WriteLine ("Введите любое число от 1 до 7: ");
+string stringNum = Console.ReadLine() ?? "0";
+
+
+int numberNum = int.Parse(stringNum);
+
+Console.SetCursorPosition(10,4);
+
+if (numberNum == 0)
+{
+    Console.WriteLine("Вы не указали номер дня недели");
+}
+if (numberNum == 1)
+{
+    Console.Write("Понедельник");
+}
+if (numberNum == 2)
+{
+    Console.Write("Вторник");
+}
+if (numberNum == 3)
+{
+    Console.Write("Среда");
+}
+if (numberNum == 4)
+{
+    Console.Write("Четверг");
+}
+if (numberNum == 5)
+{
+    Console.Write("Пятница");
+}
+if (numberNum == 6)
+{
+    Console.Write("Суббота");
+}
+if (numberNum == 7)
+{
+    Console.Write("Воскресенье");
+}
+
+
